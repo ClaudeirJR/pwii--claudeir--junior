7e7999427883d2cfa92d4c0c8e6d0369bdd6871b
Claudeir Junior <ojuniorsoares0737@gmail.com>
2025-03-25 15:10:05 -0300

    Add files via upload

---
+++ aula.php
@@ -0,0 +1,17 @@
+ <?php
+           echo "ola mundo <br>";
+
+
+$nome = "Arthur";
+
+$sobrenome = "Andrade";
+
+$numero = 8;
+
+echo "${nome} <h1> {$sobrenome} </h1>";
+
+echo $numero . "<p> " . $nome. " " .$sobrenome . "</p>";
+             ?>
+
+    </body>
+</html>
