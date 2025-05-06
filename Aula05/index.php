01c4786886589e72d403789db6fc3aec0c7b2a43
Claudeir Junior <ojuniorsoares0737@gmail.com>
2025-05-06 16:29:12 -0300

    Add files via upload

---
+++ Aula05/index.php
@@ -0,0 +1,11 @@
+<html> 
+    <body>
+
+    <form action="mensagem.php" method="POST">
+        <label for="msg">Digite sua mensagem:</label>
+     <input type="text" name="msg"><br>
+     <input type="submit">
+    </form>
+
+     </body>
+</html> 
